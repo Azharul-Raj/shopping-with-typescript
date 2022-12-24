cda5bb23608049f820ddf956c1fa581a5d00473e
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-12-24 18:44:21 +0600

    useReducer with typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 type nameInfo = {
     name: string
     message?: string
     isValid:boolean
 }
+type value = {
+    count:number
+}
 
 const Home = ({ name, message, isValid }: nameInfo) => {
-    const [count,setCount]=useState<number>(0)
+    const initialValue : value = { count: 0 }
+    const reducer = (state,action) => {
+        
+    }
+   const [state,dispatch]=useReducer(reducer,initialValue)
     return (
         <div>
-            <h2>{ count}</h2>
+            <h2>{}</h2>
             <div className="">
-                <button onClick={()=>setCount(prev=>prev+1)} className="btn">Inc</button>
+                <button onClick={()=>{}} className="btn">Inc</button>
             </div>
         </div>
     );
